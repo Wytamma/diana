435ac76e98a4d8929613a46c9c862263d42e70a4
Wytamma Wirth <wytamma.wirth@me.com>
2024-06-02 20:08:35 +1000

    :art: update git link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,7 @@
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
+					href="https://github.com/Wytamma/diana"
 					target="_blank"
 					rel="noreferrer"
 				>
