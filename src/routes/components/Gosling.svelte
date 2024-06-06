5cf21babd7e76487083c711d6ec37759c1c8793c
Wytamma Wirth <wytamma.wirth@me.com>
2024-06-06 15:28:44 +1000

    :sparkles: add download button

--- src/routes/components/Gosling.svelte
+++ src/routes/components/Gosling.svelte
@@ -2,7 +2,7 @@
     import { onMount } from 'svelte';
     import { embed } from 'gosling.js';
 	export let spec: any;
-    let api: any;
+    let goslingApi: any;
     let mounted = false;
     
     let opts = {
@@ -12,13 +12,22 @@
             'root': {'background': 'transparent'}, }
     };
     function renderGosling() {
-        embed(document.getElementById('gosling-container'), spec, opts).then((api) => {
+        let container = document.getElementById('gosling-container');
+        if (!container) {
+            return;
+        }
+        embed(container, spec, opts).then((api) => {
             console.log('Gosling API:', api);
             const trackIds = api.getTracks()
             console.log('Track IDs:', trackIds);
-
+            goslingApi = api;
         });
     }
+
+    function download() {
+        goslingApi.exportPng();
+    }
+
     $: if (spec && mounted) {
         renderGosling();
     };
@@ -36,7 +45,16 @@
 
         </div>
     </section>
-    <footer class="card-footer">
+    <footer class="flex card-footer justify-end mt-4">
+        <button type="button" class="btn btn-sm variant-filled-primary" on:click={download}>
+            <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+              </svg>
+            </span>
+            <span>
+                Download
+            </span>
+        </button>
     </footer>
 </div>
 
